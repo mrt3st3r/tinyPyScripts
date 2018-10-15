3c20744ac66d49483d96a6fcf8db17777437871c
Hamid Asgarian <hamidreza.asgarian@airnz.co.nz>
2018-10-15 13:37:18 +1300

    siteScraper initial commit

---
+++ siteScraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+def getootbalitarinatestews():
+    proxies = {
+        'http': 'http://proxy2.isis.airnz.co.nz:5865',
+        'https': 'https://proxy2.isis.airnz.co.nz:5865'
+    }
+    urls = requests.get('http://footballitarin.com/links.php')
+    urls.raise_for_status()
+    baseurl ='http://footballitarin.com/link_page.php'
+    fname = 'footbalitarin_latest_news'
+
+    data = urls.text
+    #print(data)
+    soup = BeautifulSoup(data, features='lxml')
+    latestNews = soup.find_all('div', {'class': 'news_title'})
+    # print(f'the length is {len(latestNews)}')
+    with open('/Users/asgarh/Desktop/'+fname+'.html', 'w') as f:
+        for l in latestNews:
+            for news in latestNews:
+                # print(news)
+                for n in news:
+                    # print(n)
+                     #str(n).replace('link_page.php', baseurl)
+                     f.write('<li>' + str(n).replace('link_page.php', baseurl) + '</li>')
+    print('Successfully received the latest news from Footbalitarin Website!\n\n')
+
+    with open('/Users/asgarh/Desktop/'+fname+'.html', 'r') as f:
+         for _ in f:
+            pprint(_)
+
+getootbalitarinatestews()
