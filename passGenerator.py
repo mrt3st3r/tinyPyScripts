6f87569437123c00f8aac95b3b543f5f09cf1fe4
MrT3st3r <paarsa.n@gmail.com>
2018-10-16 12:59:16 +1300

    "initial commit"

---
+++ passGenerator.py
@@ -0,0 +1,16 @@
+"""
+A simple password genrator.
+"""
+def main():
+
+    ask the user if he/she want a weak or string password
+    if weak
+        create aweak password
+    if strong
+        create a strong password
+
+
+
+__name__ =='__main__:'
+main()
+
