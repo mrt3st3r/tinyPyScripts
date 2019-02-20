aef6cfa4bbe31bccbb7977d8a9a9ccedb0eb7d10
paarsa <p.asgarian@auckland.ac.nz>
2019-02-21 09:27:34 +1300

    added new scripts

--- siteScraper.py
+++ siteScraper.py
@@ -6,25 +6,25 @@ def getootbalitarinatestews():
     urls = requests.get('http://footballitarin.com/links.php')
     urls.raise_for_status()
     baseurl ='http://footballitarin.com/link_page.php'
-    fname = 'footbalitarin_latest_news'
 
+    newsfile = 'C:\\Users\\paarsa\\Desktop\\footbalitarin_latest_news.html'
     data = urls.text
     #print(data)
     soup = BeautifulSoup(data, features='lxml')
     latestNews = soup.find_all('div', {'class': 'news_title'})
     # print(f'the length is {len(latestNews)}')
-    with open('/Users/XZY/Desktop/'+fname+'.html', 'w') as f:
+    with open(newsfile, 'w', encoding="utf-8") as f:
         for l in latestNews:
             for news in latestNews:
                 # print(news)
                 for n in news:
                     # print(n)
-                     #str(n).replace('link_page.php', baseurl)
-                     f.write('<li>' + str(n).replace('link_page.php', baseurl) + '</li>')
+                    #str(n).replace('link_page.php', baseurl)
+                    f.write("<li>" + str(n).replace('link_page.php', baseurl) + "</li>")
     print('Successfully received the latest news from Footbalitarin Website!\n\n')
-
-    with open('/Users/XZY/Desktop/'+fname+'.html', 'r') as f:
-         for _ in f:
-            pprint(_)
-
+"""
+    with open(newsfile, 'r', encoding='utf-8') as f:
+        for a in f:
+            print(a)
+"""
 getootbalitarinatestews()
