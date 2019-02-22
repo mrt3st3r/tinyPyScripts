001c295ecc0650033d01c115fbb8c0a27b38a13b
mrt3st3r <36054380+mrt3st3r@users.noreply.github.com>
2019-02-22 15:17:02 +1300

    Update siteScraper.py

--- siteScraper.py
+++ siteScraper.py
@@ -20,13 +20,10 @@ def getootbalitarinatestews():
     soup = BeautifulSoup(data, features='lxml')
     latestNews = soup.find_all('div', {'class': 'news_title'})
     # print(f'the length is {len(latestNews)}')
-<<<<<<< HEAD
     with open(newsfile, 'w', encoding="utf-8") as f:
         for l in latestNews:
-=======
     with open('/Users/asgarh/Desktop/'+fname+'.html', 'w') as f:
         for _ in latestNews:
->>>>>>> a764ef882230d0a2894bbf75cc790a6dd8e35256
             for news in latestNews:
                 # print(news)
                 for n in news:
@@ -34,18 +31,15 @@ def getootbalitarinatestews():
                     #str(n).replace('link_page.php', baseurl)
                     f.write("<li>" + str(n).replace('link_page.php', baseurl) + "</li>")
     print('Successfully received the latest news from Footbalitarin Website!\n\n')
-<<<<<<< HEAD
 """
     with open(newsfile, 'r', encoding='utf-8') as f:
         for a in f:
             print(a)
 """
-=======
 
     with open('/Users/asgarh/Desktop/'+fname+'.html', 'r') as f:
             for _ in f:
                 pprint(_)
 
 
->>>>>>> a764ef882230d0a2894bbf75cc790a6dd8e35256
 getootbalitarinatestews()
